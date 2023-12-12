7507c5966144fdb97618c6c482d1981f58cbe6b0
Mohamed Bakr <mohamed.bakr@nacitadrive.com>
2023-12-12 13:52:25 +0200

    add dot env

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+// Import all env vars from .env file
+require('dotenv').config()
+export const MY_SECRET_KEY = process.env.MY_SECRET_KEY
+console.log(MY_SECRET_KEY) // => Hello
 
 const app = express();
 

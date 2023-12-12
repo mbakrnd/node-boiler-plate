17a0bbe57e833e81d3872a8e398de7ebd590a72d
Mohamed Bakr <mohamed.bakr@nacitadrive.com>
2023-12-12 14:07:14 +0200

    add google secret manager sdk logic

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
 // Import all env vars from .env file
-require('dotenv').config()
-export const MY_SECRET_KEY = process.env.MY_SECRET_KEY
-console.log(MY_SECRET_KEY) // => Hello
+
 
 const app = express();
 
@@ -13,9 +13,28 @@ global.__basedir = __dirname;
 app.use(express.json())
 
 
+// Instantiates a client
+const client = new SecretManagerServiceClient();
+
+const name = 'projects/nacitatestproject/secrets/client-id/versions/latest';
+
+async function accessSecretVersion() {
+    const [version] = await client.accessSecretVersion({
+        name: name,
+    });
+
+    // Extract the payload as a string.
+    const payload = version.payload.data.toString();
+
+    // WARNING: Do not print the secret in a production environment - this
+    // snippet is showing how to access the secret material.
+    console.info(`Payload: ${payload}`);
+}
+
 
 
 app.get('/', async (req, res) => {
+    accessSecretVersion();
     console.log(process.env.ENV)
     res.send('Welcome to Node boiler plate!')
 });
